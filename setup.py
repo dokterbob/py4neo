316f1ad7e29267243710c0288787755e79dcb8ed
Tim McNamara <paperless@timmcnamara.co.nz>
2011-01-16 12:09:26 +1300

    setup.py tweaks.

--- setup.py
+++ setup.py
@@ -3,18 +3,7 @@ setup(
     name = "py4neo",
     version = "0.1",
     packages = find_packages(),
-
-    # Project uses reStructuredText, so ensure that the docutils get
-    # installed or upgraded on the target machine
     install_requires = ['docutils>=0.3', 'distribute'],
-    package_data = {
-        # If any package contains *.txt or *.rst files, include them:
-        '': ['*.txt', '*.rst'],
-        # And include any *.msg files found in the 'hello' package, too:
-        'hello': ['*.msg'],
-    },
-
-    # metadata for upload to PyPI
     author = "Tim McNamara",
     author_email = "paperless@timmcnamara.co.nz",
     description = "This is an Example Package",
@@ -29,9 +18,5 @@ setup(
     "License :: OSI Approved :: GNU General Public License (GPL)",
     "Operating System :: OS Independent",
     "Topic :: Database",
-    "Topic :: Utilities"
-    ],
-    long_description = open('README').read(),
-
-    # could also include long_description, download_url, classifiers, etc.
-)
+    "Topic :: Utilities"],
+    long_description = open('README').read())
